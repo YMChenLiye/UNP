478f80393fb8554738b88673e58deb8e7f6a875a
ymchenliye <ymchenliye@gmail.com>
2016-06-15 10:47:10 +0800

    finish chapter 6

---
+++ 06/strcliselect02.c
@@ -0,0 +1,44 @@
+#include "unp.h"
+
+void str_cli(FILE* fp,int sockfd)
+{
+	int 		maxfdp1, stdineof;
+	fd_set		rset;
+	char		buf[MAXLINE];
+	int			n;
+
+	stdineof = 0;
+	FD_ZERO(&rset);
+	for(; ;){
+		if(stdineof == 0){
+			FD_SET(fileno(fp),&rset);
+		}
+		FD_SET(sockfd,&rset);
+		maxfdp1 = max(fileno(fp),sockfd) + 1;
+		Select(maxfdp1,&rset,NULL,NULL,NULL);
+
+		if(FD_ISSET(sockfd,&rset)){
+			if((n = Read(sockfd,buf,MAXLINE)) == 0){
+				if(stdineof == 1){
+					return ;	//normal termination
+				}else {
+					err_quit("std_cli: server terminated prematurely");
+				}
+			}
+			Write(fileno(fp),buf,n);
+		}
+
+		if(FD_ISSET(fileno(fp),&rset)){
+			if((n = Read(fileno(fp),buf,MAXLINE)) == 0){
+				stdineof = 1;
+				Shutdown(sockfd,SHUT_WR);
+				FD_CLR(fileno(fp),&rset);
+				continue;
+			}
+
+			Write(sockfd,buf,n);
+		}
+	}
+}
+
+
